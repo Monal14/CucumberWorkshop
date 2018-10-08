52ebdb22d16215946868602757488112524164a9
monal <monal_asawa@trimble.com>
2018-10-08 10:01:41 +0530

    commit4

--- CucumberWorkshop/src/test/java/com/trimble/testing/PostRequestJavaCode/RunnerForPostRequestTest.java
+++ CucumberWorkshop/src/test/java/com/trimble/testing/PostRequestJavaCode/RunnerForPostRequestTest.java
@@ -10,12 +10,12 @@ import cucumber.api.junit.Cucumber;
 (
 features = {
 				"src/test/resources/com/trimble/testing/PostRequestJavaCode/" }, glue = {
-						"com/trimble/testing/PostRequestJavaCode/" }
-						/*plugin={"pretty",
+						"com/trimble/testing/PostRequestJavaCode/" },
+						plugin={"pretty",
 								"html:target/cucumber-htmlreport",
 								"json:target/cucumber-report6.json",
 								"com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport1.html"
-						}*/)
+						})
 public class RunnerForPostRequestTest {
 
 	
